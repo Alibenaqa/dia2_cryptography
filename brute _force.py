ef6aafb886b8f49fb0a6dfcca00df0da768f8cbc
Alirrobio <mohamedalibenaqa@gmail.com>
2025-11-12 12:21:23 +0100

    ajout du fichier brute_force.py

---
+++ brute _force.py
@@ -0,0 +1,18 @@
+import string
+
+def brute_force_cesar(texte_chiffre):
+    alphabet = string.printable
+    for decalage in range(len(alphabet)):
+        resultat = ""
+        for caractere in texte_chiffre:
+            if caractere in alphabet:
+                index = alphabet.index(caractere)
+                index_decale = (index - decalage) % len(alphabet)
+                resultat += alphabet[index_decale]
+            else:
+                resultat += caractere
+        print(f"Décalage {decalage} : {resultat}")
+
+# Exemple
+texte_chiffre = "GtsotzwQjRtsij678&"
+brute_force_cesar(texte_chiffre)
